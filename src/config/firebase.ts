435a64d321a53e3e177725106f479701198aab69
ifahadashraf <fahadashraf9612@gmail.com>
2024-10-06 22:15:09 +0200

    initial commit

---
+++ src/config/firebase.ts
@@ -0,0 +1,13 @@
+import admin from "firebase-admin";
+
+// import serviceAccount from '../firebase-service.json';
+
+// admin.initializeApp({
+//   // credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+// });
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});
+
+export default admin;
